a211c6d41f27cfbb5541f77a47d35dda33c6fb0d
github username <github email address>
2019-02-06 13:18:04 -0500

    hmm

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::API
   before_action :authorized
 
   def encode_token(payload)
-  
-    JWT.encode(payload, ENV["jwt_key"])
+
+    JWT.encode(payload, 'ilovebooba')
   end
 
   def auth_header
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::API
       token = auth_header.split(' ')[1]
       # header: { 'Authorization': 'Bearer <token>' }
       begin
-        JWT.decode(token, ENV["jwt_key"], true, algorithm: 'HS256')
+        JWT.decode(token, 'ilovebooba', true, algorithm: 'HS256')
       rescue JWT::DecodeError
         nil
       end
