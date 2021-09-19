d68ea5608affb49ecf3852d8c847d4612a204478
Ryan <omgitsgod@gmail.com>
2021-09-18 23:47:57 -0400

    Add get_topic and get_pol

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,24 @@ class ArticlesController < ApplicationController
   article = Article.create(article_params)
   end
 
+  def get_topic
+    topic = params[:topic]
+    host = ENV['news_host']
+    key = ENV['news_key']
+    url = "#{news_host}/#{topic}?sources=politico&apiKey=#{key}"
+    response = RestClient.get(url)
+    render json: response
+  end
+
+  def get_pol
+    pol = params[:pol]
+    host = ENV['news_host']
+    key = ENV['news_key']
+    url = "#{news_host}/everything?sources=politico&q=#{pol}&apiKey=#{key}"
+    response = RestClient.get(url)
+    render json: response
+  end
+
   private
 
   def article_params
