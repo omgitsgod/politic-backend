631a4c221fef697a93de4efb90f8a46513aa4b5c
Ryan <omgitsgod@gmail.com>
2021-09-21 11:28:04 -0400

    Add congress routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get '/congress', to: 'congress#get_congress'
+  get '/votes/recent', to: 'congress#get_votes'
+  get '/votes/:pol', to: 'congress#get_pol_votes'
+  get '/reps/:zip', to: 'congress#get_my_pols'
+  get '/bills', to: 'congress#get_bills'
+  get '/bills/:id', to: 'congress#get_bill'
+  get '/finance/:id/:method', to: 'congress#get_finances'
+  get '/zip/:lat/:long', to: 'zips#get_zip', constraints: { lat: /\d+\.\d+/, long: /\d+\.\d+/ }
   resources :conversations, only: [:index, :create]
   resources :messages, only: [:create]
   resources :articles
